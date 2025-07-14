9192aee454660f3469976fed0d41b114daecbfaa
Vitor Marinheiro <vitormarinheiro9@gmail.com>
2025-07-14 15:16:12 -0300

    feat: paginacao no frontend

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import { CardPost } from "@/components/CardPost";
 import styles from './page.module.css'
 import logger from "@/logger";
+import Link from "next/link";
 
-async function getAllPosts() {
-  const response = await fetch('http://localhost:3042/posts')
+async function getAllPosts(page) {
+  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`)
   if (!response.ok) {
     logger.error('Erro ao consumir API!')
     return []
@@ -12,11 +13,16 @@ async function getAllPosts() {
   return response.json()
 }
 
-export default async function Home() {
-  const posts = await getAllPosts()
+export default async function Home({ searchParams }) {
+  const currentPage = searchParams?.page || 1
+  const { data: posts, prev, next } = await getAllPosts(currentPage)
   return (
     <main className={styles.grid}>
       {posts.map(post => <CardPost key={post.id} post={post} />)}
+      <div className={styles.links}>
+        {prev && <Link href={`/?page=${prev}`}>Página Anterior</Link>}
+        {next && <Link href={`/?page=${next}`}>Próxima Página</Link>}
+      </div>
     </main>
   );
 }

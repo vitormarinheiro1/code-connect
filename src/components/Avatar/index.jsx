d9475f16ff5bbc48b5f542217a948818c0683cdb
Vitor Marinheiro <vitorrodrigues@prefeitura.sp.gov.br>
2025-07-14 10:50:43 -0300

    first commit

---
+++ src/components/Avatar/index.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image"
+
+export const Avatar = ({ name, imageSrc }) => {
+    return (
+        <ul>
+            <li><Image src={imageSrc} width={32} height={32} alt={`Avatar do(a) ${name}`} /></li>
+            <li>@{name}</li>
+        </ul>
+    )
+}

4c77f61fbf924068a26fd884142854853fd9a0e1
Vitor Marinheiro <vitormarinheiro9@gmail.com>
2025-07-15 23:02:00 -0300

    feat: adiciona pagina de not found

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -4,6 +4,7 @@ import html from 'remark-html';
 import styles from './page.module.css'
 import { CardPost } from "@/components/CardPost";
 import db from "../../../../prisma/db";
+import { redirect } from "next/navigation";
 
 async function getPostBySlug(slug) {
     // const url = `http://localhost:3042/posts?slug=${slug}`
@@ -19,23 +20,35 @@ async function getPostBySlug(slug) {
     // }
     // const post = data[0]
 
-    const post = await db.post.findFirst({
-        where: {
-            slug
-        },
-        include: {
-            author: true
+    try {
+        const post = await db.post.findFirst({
+            where: {
+                slug
+            },
+            include: {
+                author: true
+            }
+        })
+
+        if (!post) {
+            throw new Error(`Post com o slug ${slug} não foi encontrado`)
         }
-    })
 
-    const processedContent = await remark()
-        .use(html)
-        .process(post.markdown);
-    const contentHtml = processedContent.toString();
+        const processedContent = await remark()
+            .use(html)
+            .process(post.markdown);
+        const contentHtml = processedContent.toString();
 
-    post.markdown = contentHtml
+        post.markdown = contentHtml
 
-    return post
+        return post
+    } catch (error) {
+        logger.error('Falha ao obter o post com o slug: ', {
+            slug,
+            error
+        })
+    }
+    redirect('/not-found')
 }
 
 const PagePost = async ({ params }) => {
